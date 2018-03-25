df73818f111b4bf8f625c9ed5612bb7df8899338
caojunhui <770541944@qq.com>
2018-03-26 00:21:50 +0800

    无用代码

--- demo.py
+++
@@ -1,8 +0,0 @@
-class abcd(object):
-    def __init__(self):
-        print('__init__(self)')
-
-    def __init__(self,**kargs):
-        print('__init__(self,**kargs):')
-        for i in kargs:
-            print(i)
