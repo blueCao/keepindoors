cdaebf2ec39a22cd74afe8144a8ee68b38afa633
caojunhui <770541944@qq.com>
2018-03-29 13:17:43 +0800

    str,bytes,utf8格式互相转换

---
+++ str2bytes2utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def str2utf8(s):
+    """
+    type string convert to utf8 unicode string
+    :param s: the input string
+    :return: the output unicode utf_8 string
+    """
+    assert isinstance(s,str)
+    return s.encode("unicode_escape").decode("utf-8")
+
+def pattern2utf8(p):
+    """
+    pattern string for regex match
+    :param p: the original pattern string
+    :return: the replaced match
+    """
+    assert isinstance(p,str)
+    return str2utf8(p).replace("\\","\\\\")
+
+def utf2str(u):
+    """
+    utf8 string convert to str
+    :param u: utf8 based str
+    :return: converted str
+    """
+    assert isinstance(u,str)
+    return u.encode("utf_8").decode("unicode_escape")
