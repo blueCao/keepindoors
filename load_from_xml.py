a2830022c49d9329e1f82fad74e188da31a27885
caojunhui <770541944@qq.com>
2018-03-29 18:56:56 +0800

    将带k个关键字和权重的xml新闻数据导入内存

--- load_from_xml.py
+++ load_from_xml.py
@@ -1,6 +1,4 @@
 import xml_handler as h
 
 # load data into memory
-handler = h.load_data_from_xml("output_samples_20120615_to_20120615.xml")
-# remove the duplicated data
-unique_list = h.unique(handler.doc_list)
+handler = h.load_data_from_xml("textrank.xml")
