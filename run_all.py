d24959fe6762b08caf246c89e4f13cbae3f0a193
caojunhui <770541944@qq.com>
2018-03-31 03:32:13 +0800

    启动脚本：增加时间记录

--- run_all.py
+++ run_all.py
@@ -1,20 +1,54 @@
 import xml_handler as h
 import simhash
+import datetime
+
+"""
+the running script of the project
+"""
+
+def show_time(msg):
+    """
+    print current time and a message,return current time_stamp
+    :param msg:
+    :return: current time_stamp
+    """
+    current = datetime.datetime.now()
+    print(current)
+    print(msg)
+    print()
+    return current
+
+# show begin time
+begin_time_stamp=show_time("begin")
 
 # load data into memory
 handler = h.load_data_from_xml("textrank.xml")
 
+# show minmash begin time
+begin_minhash_time_stamp=show_time("begin minhash caculate")
+
+# caculate simhash of each doc
+doc_size = len(handler.doc_list)
+threshold = 0
+simhashes = []
+for index_a in range(doc_size):
+    simhashes.append(simhash.simhash(handler.doc_list[index_a].textrank, threshold))
+
+# show minmash distances caculate begin time
+begin_distances_time_stamp=show_time("begin minhash distances caculate")
+
 #  caculate each pair distance of doc
 #  distances is a 2D array
-threshold = 0
 distances = []
-doc_size = len(handler.doc_list)
 for index_a in range(doc_size):
     distances.append([])
     for index_b in range(index_a+1,doc_size):
-        simhash_a = simhash.simhash(handler.doc_list[index_a].textrank, threshold)
-        simhash_b = simhash.simhash(handler.doc_list[index_b].textrank, threshold)
+        simhash_a = simhashes[index_a]
+        simhash_b = simhashes[index_b]
         distances[index_a].append(simhash.distance(simhash_a,simhash_b))
+
+# show minmash distances caculate begin time
+end_time_stamp=show_time("all finished!")
 
 # display the similar doc
 min_distance = 6
