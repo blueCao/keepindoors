a1dc45a098e84117fe40c57d4957f195277be8c4
Ove <770541944@qq.com>
2018-05-28 00:07:57 +0800

    更正低级错误

--- spark-submit-jobs/connectedComponentsJob.py
+++ spark-submit-jobs/connectedComponentsJob.py
@@ -20,6 +20,9 @@ def main():
     cursor = mongo.getCollection(cli,"keepindoors","docs").find()
     i = 0
     for r in cursor:
+        # del "_id" key which will throws error when createDataFrame
+        if "_id" in r.keys():
+            del r["_id"]
         r["id"] = i
         r["name"] = str(i)
         localVertices.append(r)
@@ -28,13 +31,18 @@ def main():
     # e
     localEdges = []
     cursor = mongo.getCollection(cli, "keepindoors", "doc_pairs").find()
-    for r in cursor:
-        localEdges.append(r)
+    # for r in cursor:
+    #     # del "_id" key which will throws error when createDataFrame
+    #     if "_id" in r.keys():
+    #         del r["_id"]
+    #     localEdges.append(r["same"])
+    localEdges = cursor.next()["same"]
 
     v = spark.createDataFrame(localVertices)
     e = spark.createDataFrame(localEdges, ["distance","src", "dst"])
     g = GraphFrame(v,e)
-    spark.setCheckpointDir("/tmp/spark/checkpoint")
+    # get sparkContext from sparkSession
+    spark.sparkContext.setCheckpointDir("/tmp/spark/checkpoint")
     result = g.connectedComponents()
 
     # save in hdfs
