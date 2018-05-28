751db6ea09f4a33dd81473b7861d19c229788d43
Ove <770541944@qq.com>
2018-05-29 01:19:47 +0800

    更新

--- spark-submit-jobs/connectedComponentsJob.py
+++ spark-submit-jobs/connectedComponentsJob.py
@@ -23,30 +23,34 @@ def main():
         # del "_id" key which will throws error when createDataFrame
         if "_id" in r.keys():
             del r["_id"]
-        r["id"] = i
+        if "simhash" in r.keys():
+            del r["simhash"]
+        r["id"] = r["docno"]
         r["name"] = str(i)
         localVertices.append(r)
         i = i + 1
 
     # e
     localEdges = []
-    cursor = mongo.getCollection(cli, "keepindoors", "doc_pairs").find()
+    cursor = mongo.getCollection(cli, "keepindoors", "distances").find()
     # for r in cursor:
     #     # del "_id" key which will throws error when createDataFrame
     #     if "_id" in r.keys():
     #         del r["_id"]
     #     localEdges.append(r["same"])
-    localEdges = cursor.next()["same"]
+    localEdges = []
+    for r in cursor:
+        localEdges.append((r["docno1"],r["docno2"],r["distance"]))
 
     v = spark.createDataFrame(localVertices)
-    e = spark.createDataFrame(localEdges, ["distance","src", "dst"])
+    e = spark.createDataFrame(localEdges, ["src", "dst","distance"])
     g = GraphFrame(v,e)
     # get sparkContext from sparkSession
     spark.sparkContext.setCheckpointDir("/tmp/spark/checkpoint")
     result = g.connectedComponents()
 
     # save in hdfs
-    result.write.format("csv").save("/tmp/spark/graphframes.csv")
+    result.write.format("json").save("/tmp/spark/graphframes.json")
 
 if __name__ == "__main__":
     main()
